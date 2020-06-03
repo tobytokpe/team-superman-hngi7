a9162fd1a6ab775a630ce772c2a4cec489707d5c
Olowu Oluwatobi Tope <39678603+tobytokpe@users.noreply.github.com>
2020-06-03 10:08:40 +0100

    Create OluwatobiOlowu.js

---
+++ scripts/OluwatobiOlowu.js
@@ -0,0 +1,8 @@
+function Task2(Name, HNGi7ID, Language, Email) 
+	{
+		console.log
+			(
+    	`			Hello World, this is Oluwatobi Olowu with HNGi7 ID HNG-02668 using Javascript for stage 2 task`
+  			);
+	}
+Task2("Oluwatobi Olowu", "HNG-02668", "Javascript", "tobytokpe@gmail.com");
